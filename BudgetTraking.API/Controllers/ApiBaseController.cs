d901f7e29391cdda22dfb91fc946d11759ce51a5
Mohammed Magdi <dev.mmagdi@yahoo.com>
2018-09-14 01:20:44 +0200

    add api

---
+++ BudgetTraking.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,15 @@
+﻿using BudgetTracking.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BudgetTraking.API.Controllers
+{
+    public class ApiBaseController : ApiController
+    {
+        public BudgetTrackingEntities db = new BudgetTrackingEntities(); 
+    }
+}
---
+++ BudgetTraking.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BudgetTraking.API.WebApiApplication" Language="C#" %>
